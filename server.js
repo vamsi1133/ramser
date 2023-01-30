a8d83ed10833e453d66cca2bff4e76dc91b1435b
vamsi krishna raju pericherla <vamsiraju113@gmail.com>
2023-01-30 10:53:59 +0530

    a

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/msg', (req, res) => {
+  res.send('Hello World');
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});

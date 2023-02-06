de3cdf418d10387109d154f775f60fcbb39cb757
vamsi krishna raju pericherla <vamsiraju113@gmail.com>
2023-02-06 11:25:11 +0530

    second commit

---
+++ db.js
@@ -0,0 +1,5 @@
+module.exports.userdata = [
+  { username: "ram", password: "1234" },
+  { username: "vamsi", password: "1235" },
+  { username: "raju", password: "12d4" },
+];
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-const http = require("http");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
-app.get("/msg", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+
+const users = [
+  {
+    username: "ram",
+    password: "12345",
+  },
+  {
+    username: "vamsi",
+    password: "12345",
+  },
+];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) {
+    return res.status(401).send({ message: "Incorrect username or password" });
+  }
+
+  return res.send({ message: "Login successful" });
 });
 
-const server = http.createServer(app);
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8000, () => {
+  console.log("Listening on port 8000");
 });
